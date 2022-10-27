454fcf62e1a4e36ec72736bdf21d1f7d47e7dfc3
Derek Wilson <u4hcsdaw@localhost>
2022-10-27 11:26:52 -0400

    This is an original commit of Laboratory #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Menuitem } from './models/menuitem';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  title = 'Chirpus Pizza';
+
+  menu: Menuitem[] =[
+    {id:0,  item: "pizza",  category:"pizza",  price:2.99},
+    {id:1,  item: "more pizza",  category:"pizza",  price:3.99},
+    {id:2,  item: "beer",  category:"beer",  price:6.99},
+    {id:0,  item: "more beer",  category:"beer",  price:8.99},
+    {id:0,  item: "something called a salad",  category:"vegetarian",  price:2.99},
+    {id:0,  item: "potato",  category:"is potato",  price:2.99},
+    
+  ]
+
 }
---
+++ src/app/models/menuitem.ts
@@ -0,0 +1,7 @@
+export interface Menuitem {
+
+    id: number;
+    item: string;
+    category:string;
+    price: number;
+}
